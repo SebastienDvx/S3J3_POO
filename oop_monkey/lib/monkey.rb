46b928ab3edd64389d400da15d06b5431424e9ef
Sébastien DEVAUX <devsebastien@hotmail.fr>
2018-01-24 18:42:43 +0100

    [MAJ] Monkeys

--- oop_monkey/lib/monkey.rb
+++ oop_monkey/lib/monkey.rb
@@ -9,15 +9,17 @@ class Monkey
     @foods_eaten = []
   end
 
-  def eat(food)
+  def eat(*food)
+    # p @foods_eaten
     @foods_eaten << food
+    # p @foods_eaten
   end
 
   def introduce
     if @foods_eaten == []
       puts "Hi ! My name is #{@name}, and I am a #{@species}."
     else
-      puts "Hi ! My name is #{@name}, I am a #{@species}, and I eat #{@foods_eaten}"
+      puts "Hi ! My name is #{@name}, I am a #{@species}, and I eat #{@foods_eaten.join(', ')}"
     end
   end
 
@@ -25,17 +27,13 @@ end
 
 georges = Monkey.new("Georges", "Grizzly")
 
-georges.introduce
+# georges.introduce
 
-georges.eat("bananas")
-
-georges.introduce
-
-georges.eat("grappes")
-
-p georges.foods_eaten
+georges.eat("bananas", "grapes")
 
 georges.introduce
 
-
+# georges.eat("grappes")
+# p georges.foods_eaten
+# georges.introduce
 # p @foods_eaten.to_s
