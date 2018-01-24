6942167e3dc5e2d067f560f488ee32787a59eb7b
Sébastien DEVAUX <devsebastien@hotmail.fr>
2018-01-24 16:49:40 +0100

    exo students [DONE]

--- students_class/students.rb
+++ students_class/students.rb
@@ -1,5 +1,15 @@
 class Students
 
+  attr_accessor :name
+  attr_reader :age
 
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
 
 end
+
+seb = Students.new("Sebastien", 24)
+p seb.name
+p seb.age
